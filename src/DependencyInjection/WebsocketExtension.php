66a8775609f37f1d47cf6283e851ac8dec1c02f4
RANDRIAMITSIRY Lovasoa Gabriel <gabriel@ipedis.com>
2020-02-19 16:32:14 +0400

    add tag for channels

--- src/DependencyInjection/WebsocketExtension.php
+++ src/DependencyInjection/WebsocketExtension.php
@@ -1,6 +1,7 @@
 <?php
 namespace Ipedis\Bundle\Websocket\DependencyInjection;
 
+use Ipedis\Bundle\Websocket\Channel\Contract\ChannelInterface;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
@@ -23,10 +24,21 @@ class WebsocketExtension extends Extension
         );
 
         $loader->load('services.yaml');
+        $this->addWebsocketChannelTag($container);
     }
 
     public function getAlias()
     {
         return 'ipedis_websocket';
+    }
+
+    /**
+     * Automatically add tag ps.websocket_channel for all class which implement ChannelInterface
+     * @param ContainerBuilder $containerBuilder
+     */
+    protected function addWebsocketChannelTag(ContainerBuilder $containerBuilder)
+    {
+        $containerBuilder->registerForAutoconfiguration(ChannelInterface::class)
+            ->addTag('ps.websocket_channel');
     }
 }
