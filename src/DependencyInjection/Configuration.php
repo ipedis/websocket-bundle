13d659cf0b63864297513d4a90480c3ab3318126
RANDRIAMITSIRY Lovasoa Gabriel <gabriel@ipedis.com>
2020-02-19 15:14:38 +0400

    add configuration

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -1,6 +1,7 @@
 <?php
 
 
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
 class Configuration implements ConfigurationInterface
@@ -10,6 +11,20 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        // TODO: Implement getConfigTreeBuilder() method.
+        $treeBuilder = new TreeBuilder('ipedis_websocket');
+        $treeBuilder->getRootNode()
+            ->children()
+               ->arrayNode('connection')
+                  ->children()
+                      ->scalarNode('websocket_remote_host')->defaultValue('localhost')->end()
+                      ->integerNode('websocket_remote_port')->defaultValue(8081)->end()
+                      ->scalarNode('websocket_host')->defaultValue('127.0.0.1')->end()
+                      ->integerNode('websocket_port')->defaultValue(8081)->end()
+                      ->scalarNode('websocket_remote_protocol')->defaultValue('ws')->end()
+                  ->end()
+               ->end()
+            ->end();
+
+        return $treeBuilder;
     }
 }
